3d45f3eb6b9f73eb6871ced658c49d568343380c
Wes <virtuallyaverage@gmail.com>
2023-04-07 22:39:48 -0500

    khjgf jk;l dsgfsdljg;kh

--- format_outline.py
+++ format_outline.py
@@ -11,18 +11,31 @@ with open(element_filename, "r") as file:
 init()  
 print(Fore.BLUE+f"\n{'*'*109}\n\n\tCreate the outline prompt\n\n{'*'*109}\n"+ Style.RESET_ALL)
 
-points_for_each_section = "Plot points: significant events that move the story or change the story direction. Plot points can include things like the inciting incident, the midpoint reversal, the climax, and the resolution. Character points: key moments or decisions in a story that reveal something important about a character's personality, motivations, or development. Theme points: recurring ideas or messages in a story that give it deeper meaning or resonance."
+points_for_each_section = "Plot points: significant events that move the story or change the story direction. Plot points can include things like the inciting incident, the midpoint reversal, the climax, and the resolution. Character points: key moments or decisions in a story that reveal something important about a character's personality, motivations, or development. Theme points: recurring ideas or messages in a story that gives it deeper meaning or resonance."
 
 #intitialise the prompt
-prompt = f"Here is some specifics about a story I am writing. Considering ALL information given, write a detailed outline with 5 bullet points. Do this by writing the bullet points, with a # before each, then expand on any topics covered in that section, organized into categories: {points_for_each_section}"
+prompt = f"Here are some specifics about a story I am writing. Considering ALL information given, write a detailed outline with 5 bullet points. Do this by writing the bullet points, with a # before each, then expand on any topics covered in that section, organized into categories: {points_for_each_section}\n"
 
 # get a list of all of the elements
 data_string = "Info:\n"
-for category in whole_database:
-    data_string += f"{category}, ["
-    for element in whole_database[category]:
-        pass
-    data_string += "]"
-    
-print(prompt)
-        
+for category_name, _ in whole_database.items():
+    data_string += f"{category_name}, ["
+    for element_name, _ in whole_database[category_name].items():
+        working_element = whole_database[category_name][element_name]
+        if len(working_element['sub_fields']) <= 0:
+            data_string += f"{element_name}: {working_element['input']}, "
+        else:
+            data_string += f"{element_name}: "
+            for subfield_name in working_element['sub_fields']:
+                subfield = working_element['sub_fields'][subfield_name]
+                data_string += f"{subfield_name}> {subfield['input']}, "
+                
+    data_string += "]\n"
+
+#add the data to the prompt
+prompt += data_string
+
+#have it deal with none fields
+prompt += "Any data fields filled with \"None\" estimate an answer that fits the rest of the information and use that\n"
+
+print(prompt)       
